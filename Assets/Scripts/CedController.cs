3210df32610b84f9ac796c393ee95f31b23f5ba6
mkislenko <mkislenko@gmail.com>
2018-08-22 17:27:35 +0300

    newScript

---
+++ Assets/Scripts/CedController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CedController : MonoBehaviour {
+
+    public CedController instance;
+
+
+    private Animator anim;
+    private AudioSource aSource;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    
+	public void Stop()
+    {
+        anim.StopPlayback();
+        aSource.Pause();
+    }
+
+    public void Play()
+    {
+        anim.StartPlayback();
+    }
+}
---
+++ Assets/Scripts/CedVuforiaBehavior.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CedVuforiaBehavior : DefaultTrackableEventHandler
+{
+
+    private void Awake()
+    {
+        var audioSources = GetComponentsInChildren<AudioSource>();
+        var animators = GetComponentsInChildren<Animator>();
+
+        foreach (AudioSource audSource in audioSources)
+        {
+            audSource.Pause();
+        }
+
+        foreach (Animator anim in animators)
+        {
+            anim.StartPlayback();
+        }
+    }
+    protected override void Start()
+    {
+        base.Start();
+    }
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+
+        var audioSources = GetComponentsInChildren<AudioSource>();
+        var animators = GetComponentsInChildren<Animator>();
+
+        foreach(AudioSource audSource in audioSources)
+        {
+            audSource.Play();
+        }
+
+        foreach(Animator anim in animators)
+        {
+            anim.StopPlayback();
+        }
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+
+        var audioSources = GetComponentsInChildren<AudioSource>();
+        var animators = GetComponentsInChildren<Animator>();
+
+        foreach (AudioSource audSource in audioSources)
+        {
+            audSource.Pause();
+        }
+
+        foreach (Animator anim in animators)
+        {
+            anim.StopPlayback();
+        }
+    }
+}
